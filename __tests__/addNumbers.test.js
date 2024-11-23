2ed8a68520994d716f13f502d9ab64c041e764c1
Puneet Pandey <puneet@MacBookAir.lan>
2024-11-23 15:37:17 +0530

    Added Support for negative numbers check

--- __tests__/addNumbers.test.js
+++ __tests__/addNumbers.test.js
@@ -8,4 +8,10 @@ describe("Add numbers function test suits", () => {
     it("Should return 0 if no numaric value is provided in string", () => {
         expect(addNumbers("//&$")).toBe(0)
     })
+    it("Should throw error  if negative value is provided in string", () => {
+        const resFn = () => {
+            addNumbers("//1;-2,3")
+        }
+        return expect(resFn).toThrowError('negatives not allowed: -2')
+    })
 })
--- addNumbers.js
+++ addNumbers.js
@@ -7,4 +7,15 @@ export function addNumbers(numbers) {
     const matches = numberString.match(regex);
     //Step 3 If no numbers found return 0
     if (!matches) return 0;
+    //Step 4 Convert extracted numbers array into base numbers
+    let numbersArray = matches.map(num => parseInt(num, 10));
+    //Step5 Check if Numbers array have negative data
+
+    let negativeArr = numbersArray.filter(el => el < 0)
+    //Step 6 Throw Error if found
+    if (negativeArr.length > 0) {
+        throw new Error('negatives not allowed: ' + negativeArr.join(', '));
+    }
+
 }
+
